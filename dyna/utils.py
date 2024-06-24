da47f7b59d4ca72a948eb2cc61ad068dc5cc2648
AnnaZou <AnnaZou1103@gmail.com>
2024-06-23 20:07:11 -0400

    add judge

--- dyna/utils.py
+++ dyna/utils.py
@@ -1,3 +1,5 @@
+import itertools
+
 from pycocotools.coco import COCO
 
 coco17_instance = COCO("data/coco/annotations/instances_val2017.json")
@@ -11,17 +13,19 @@ id_name_mapping14 = {cat["id"]: cat["name"] for cat in cats14}
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from nltk.corpus import wordnet as wn
 
 load_dotenv(".env")
 
 client = OpenAI()
 
+
 def call_chatgpt(messages, model_name="gpt-4o"):
     # messages = [
-        #     {"role": "system", "content": "You are a helpful assistant."},
-        #     {"role": "user", "content": message}
-        # ]
-        
+    #     {"role": "system", "content": "You are a helpful assistant."},
+    #     {"role": "user", "content": message}
+    # ]
+
     completion = client.chat.completions.create(
         model=model_name,
         messages=messages
@@ -29,22 +33,26 @@ def call_chatgpt(messages, model_name="gpt-4o"):
     return completion.choices[0].message
 
 
-
-
 def load_coco2017(img_id):
     img = coco17_instance.loadImgs(img_id)[0]
-    
+
     annIds = coco17_instance.getAnnIds(imgIds=img_id, iscrowd=None)
     instance_anns = coco17_instance.loadAnns(coco17_instance.getAnnIds(imgIds=img_id, iscrowd=None))
     caption_anns = coco17_caption.loadAnns(coco17_caption.getAnnIds(imgIds=img['id']))
-    
+
     return {
         "file_name": img["file_name"],
         "instances": [
-                {"category": id_name_mapping17[instance["category_id"]], "bbox": instance["bbox"], "pixel_area": instance["area"]} for instance in instance_anns
-            ],
+            {"category": id_name_mapping17[instance["category_id"]], "bbox": instance["bbox"],
+             "pixel_area": instance["area"]} for instance in instance_anns
+        ],
         "captions": [
-                caption["caption"] for caption in caption_anns
-            ]
+            caption["caption"] for caption in caption_anns
+        ]
     }
-    
+
+
+def get_synset(word):
+    syn_list = wn.synonyms(word)
+    syn_list = list(itertools.chain.from_iterable(syn_list))
+    return [word]+[' '.join(syn.split('_')) for syn in syn_list]
--- judge.py
+++ judge.py
@@ -1,8 +1,8 @@
 import os
 import sng_parser
-from dyna.utils import load_coco2017
+from dyna.utils import load_coco2017, get_synset
 
-det_list = ['a', 'the', 'an', '']
+det_list = ['a', 'the', 'an', 'this', 'that', 'these', 'those', '']
 
 
 def reformat_graph(graph):
@@ -42,42 +42,55 @@ if __name__ == "__main__":
     gt_graph = sng_parser.parse(gt_caption)
     gt_entities, gt_relations = reformat_graph(gt_graph)
 
-    vlm_output = ("the image features a man riding a skateboard down a path, accompanied by his dog the man is wearing a "
-                  "green jacket and appears to be enjoying the outdoor activity the dog is walking beside him, "
-                  "keeping pace with the skateboarder the scene takes place in a park-like setting, with several cars "
-                  "parked along the path there are at least nine cars visible in the background, varying in size and "
-                  "distance from the skateboarder and his dog the overall atmosphere of the image is lively and active, "
-                  "showcasing the man and his dog engaging in a fun and healthy outdoor activity.")
+    vlm_output = (
+        "A person riding a skate board with a dog following beside. the image features a man riding a skateboard down a path, accompanied by his dog the man is wearing a "
+        "green jacket and appears to be enjoying the outdoor activity the dog is walking beside him, "
+        "keeping pace with the skateboarder the scene takes place in a park-like setting, with several cars "
+        "parked along the path there are at least nine cars visible in the background, varying in size and "
+        "distance from the skateboarder and his dog the overall atmosphere of the image is lively and active, "
+        "showcasing the man and his dog engaging in a fun and healthy outdoor activity.")
 
     vlm_graph = sng_parser.parse(vlm_output)
     vlm_entities, vlm_relations = reformat_graph(vlm_graph)
 
     object_hallucination_num = 0
+    object_uncertain_num = 0
     relation_hallucination_num = 0
     for object_name, attribute_list in vlm_entities.items():
-        if object_name in gt_entities.keys():
-            gt_attribute_list = gt_entities[object_name]
-            for attribute in attribute_list:
-                if attribute not in gt_attribute_list:
-                    object_hallucination_num += 1
-                    # print('Hallucination')
-                    # print('vlm', object_name, attribute_list)
-                    # print('gt', object_name, gt_attribute_list)
-                    # print('\n')
-                    break
+        object_syn_list = get_synset(object_name)
+        common_objects = set(object_syn_list).intersection(gt_entities.keys())
+        if common_objects:
+            for object_synonym in common_objects:
+                gt_attribute_list = gt_entities[object_synonym]
+                for attribute in attribute_list:
+                    attribute_syn_list = get_synset(attribute)
+                    common_attributes = set(attribute_syn_list).intersection(gt_attribute_list)
+                    if not common_attributes:
+                        object_hallucination_num += 1
+                        print('Hallucination')
+                        # object_uncertain_num += 1
+                        # print('Uncertain')
+                        print('vlm', object_name, attribute_list)
+                        print('gt', object_name, gt_attribute_list)
+                        print('\n')
+                        break
         else:
-            object_hallucination_num +=1
-            # print('Hallucination')
-            # print('vlm', object_name, attribute_list)
-            # print('\n')
+            object_uncertain_num += 1
+            print('Uncertain')
+            print('vlm', object_name, attribute_list)
+            print('\n')
 
     for relation in vlm_relations:
         if relation not in gt_relations:
             relation_hallucination_num += 1
+            # TO DO
             # print('vlm relation', relation)
 
+    print('vlm_graph')
     sng_parser.tprint(vlm_graph)
+    print('gt_graph')
     sng_parser.tprint(gt_graph)
 
     print('object_hallucination_num:', object_hallucination_num)
     print('relation_hallucination_num:', relation_hallucination_num)
+    print('object_uncertain_num:', object_uncertain_num)
